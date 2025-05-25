7a3a45f79ad2e1cf1f0bc381a19d96341f1e5c6d
prltgianpaolo <LaTuaEmail>
2025-05-25 18:10:40 +0200

    Initial setup

---
+++ src/main/java/com/coachconnect/coachconnect/CoachconnectApplication.java
@@ -0,0 +1,13 @@
+package com.coachconnect.coachconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coachconnect/coachconnect/CoachconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coachconnect.coachconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
